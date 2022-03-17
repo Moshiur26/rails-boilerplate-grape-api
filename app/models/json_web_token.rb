0db04fc5c3d561775843e5e6e341f30d9b26142b
moshiur26 <moshiurrahmanb11@gmail.com>
2022-03-17 15:28:35 +0600

    create json_web_token file into model folder

---
+++ app/models/json_web_token.rb
@@ -0,0 +1,35 @@
+class JsonWebToken
+  def self.encode(payload)
+    expiration = 1.year.from_now.to_i
+    JWT.encode payload.merge(exp: expiration), Rails.application.secret_key_base
+  end
+
+  def self.decode(token)
+    JWT.decode(token, Rails.application.secret_key_base).first
+  end
+
+  def self.remove_token(bearer_token = '')
+    auth_key = AuthorizationKey.find_by(token: bearer_token) if bearer_token.present?
+    auth_key.destroy! if auth_key.present?
+    auth_key.present? ? auth_key.destroyed? : false
+  end
+
+  def self.user_token_encode(user, bearer_token = '')
+    new_has = token_hash(user)
+    auth_key = AuthorizationKey.find_by(token: bearer_token) if bearer_token.present?
+    auth_key.present? && auth_key&.authable_id == user&.id ? auth_key.update!(expiry: new_has[:expiry], token: new_has[:token]) : AuthorizationKey.create!(new_has)
+    new_has[:token]
+  end
+
+  def self.new_auth_key(user)
+    AuthorizationKey.create!(token_hash(user))
+  end
+
+  def self.token_hash(user)
+    expiry = 3.month.from_now
+    rand = SecureRandom.hex(10)
+    hash = BCrypt::Password.create(rand)
+    token = JWT.encode hash, Rails.application.secret_key_base
+    { expiry: expiry, token: token, authable: user }
+  end
+end
