ac15c213d3c74e96d7c48a48fd9211c612a6b34a
moshiur26 <moshiurrahmanb11@gmail.com>
2022-04-03 01:12:57 +0600

    added new methond single_login_token_encode into jwon_web_token

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -21,6 +21,12 @@ class JsonWebToken
     new_has[:token]
   end
 
+  def self.single_login_token_encode(user)
+    new_has = token_hash(user)
+    user&.authorization_key&.present? ? user.authorization_key.update!(token: new_has[:token], expiry: new_has[:expiry]) : AuthorizationKey.create!(token: new_has[:token], expiry: new_has[:expiry], authable: user)
+    token
+  end
+
   def self.new_auth_key(user)
     AuthorizationKey.create!(token_hash(user))
   end
